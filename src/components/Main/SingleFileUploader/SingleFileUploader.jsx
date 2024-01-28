0b452e37b174ac048263e903c7d4fd634f0f5a21
rsmax <fordrsmax@gmail.com>
2024-01-28 17:50:28 +0100

    some format

--- src/components/Main/SingleFileUploader/SingleFileUploader.jsx
+++ src/components/Main/SingleFileUploader/SingleFileUploader.jsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
+import toTimeTrack from "../../../utils/formatter";
 
 const SingleFileUploader = () => {
 
   const [sesionfile, setSesionFile] = useState(null);
+
   const [sesionObj, setSesionObj] = useState(null);
 
   const handleFileChange = (e) => {
     if (e.target.files) {
       const file0 = e.target.files[0]
-      console.log('manual load : ', file0);
-
+      // console.log('manual load : ', file0);
       setSesionFile(file0);
     }
   };
@@ -18,8 +19,7 @@ const SingleFileUploader = () => {
     e.preventDefault()
     const [item] = e.dataTransfer.items;
     const fileDroped = item.getAsFile()
-
-    console.log('Droped load : ', fileDroped);
+    // console.log('Droped load : ', fileDroped);
     setSesionFile(fileDroped);
   }
 
@@ -35,16 +35,64 @@ const SingleFileUploader = () => {
           mapedLaps[item.car] = group;
           return mapedLaps;
         }, {});
-        console.log('vueltas : ',mapedLapsByDriver);
+        // console.log('vueltas : ', mapedLapsByDriver);
+        setSesionObj(mapedLapsByDriver)
+
       }
+
     }
     fetchData();
   }, [sesionfile])
 
+  const drawList = () => {
+
+    const myList = []
+
+    const buildCols = (laps) => {
+
+      const cols = []
+
+      for (const l of laps) {
+        const element = <td>
+          {toTimeTrack(l.time)}
+        </td>
+        cols.push(element)
+      }
+
+      return cols
+
+    }
+
+    for (const key in sesionObj) {
+      if (Object.hasOwnProperty.call(sesionObj, key)) {
+        
+        const element =
+          <tr>
+            <td>
+              {key.toString()}
+            </td>
+            {buildCols(sesionObj[key])}
+          </tr>;
+
+        myList.push(element);
+
+      }
+    }
+    return myList
+  }
+
   return (
     <>
       <div className="SingleFileUploader">
-        {sesionObj?<></>:<input className="dropAerea" id="file" type="file" onChange={handleFileChange} onDrop={handleDrop} />}
+        {sesionObj ? <>
+          <table className="timeTable">
+            <th>
+              <td>driver</td>
+            </th>
+            {drawList()}
+          </table>
+        </>
+          : <input className="dropAerea" id="file" type="file" onChange={handleFileChange} onDrop={handleDrop} />}
       </div>
     </>
   );
--- src/utils/formatter.jsx
+++ src/utils/formatter.jsx
@@ -1,7 +1,9 @@
-const toAsteriscString = (num) => {
-  num = num.toString();
-  while (num.length < 4) num = "0" + num;
-  return "#" + num;
+
+
+const toTimeTrack = (num) => {
+  let myTime = new Date(num);
+  let formatedTime = myTime.getMinutes() + ':' + myTime.getSeconds() + '.' +  myTime.getMilliseconds()
+  return formatedTime
 }
 
-export default toAsteriscString;
+export default toTimeTrack;
